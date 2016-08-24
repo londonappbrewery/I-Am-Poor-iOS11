0c5a30fb0a21a75a2b6b1f46a1d5fb71af0ce151
angelabauer <angela_yu@hotmail.co.uk>
2016-08-24 14:24:29 +0100

    updated to Swift 3 Xcode 8

---
+++ I Am Poor/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  I Am Poor
+//
+//  Created by Angela Yu on 24/08/2016.
+//
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
